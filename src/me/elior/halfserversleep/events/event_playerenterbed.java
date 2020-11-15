f348cdeb859bda9cc83e29d5e42a1ea4cd7d8a43
Elior <50843891+Elior2739@users.noreply.github.com>
2020-11-15 03:50:05 +0200

    HalfServerSleep Update 1.0.4
    Added - Option to cancel Rain or Thunder in the config file

--- src/me/elior/halfserversleep/events/event_playerenterbed.java
+++ src/me/elior/halfserversleep/events/event_playerenterbed.java
@@ -1,6 +1,7 @@
 package me.elior.halfserversleep.events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 
 import me.elior.halfserversleep.Main;
@@ -14,18 +15,21 @@ public class event_playerenterbed {
 		final int neednum = (Bukkit.getWorld(utils.Settings.reguler_world).getPlayers().size() / 2 == 0) ? 1 : Bukkit.getWorld(utils.Settings.reguler_world).getPlayers().size() / 2;
 		Bukkit.broadcastMessage(utils.PlaceholderString(utils.Messages.player_went_to_sleep, e.getPlayer(), utils.playersSleeping, neednum));
 		if(utils.playersSleeping >= neednum && !utils.isReachedToNeeded) {	
-			utils.isReachedToNeeded = true;
 			Bukkit.broadcastMessage(utils.PlaceholderString(utils.Messages.all_needed_players_sleep, e.getPlayer(), utils.playersSleeping, neednum));
+			utils.isReachedToNeeded = true;
 			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.getPlugin(Main.class), new Runnable() { public void run() {
 				if(utils.playersSleeping >= neednum) {
-					Bukkit.getWorld(utils.Settings.reguler_world).setTime(utils.Settings.time_before_change);
+					World world = Bukkit.getWorld(utils.Settings.reguler_world);
+					world.setTime(utils.Settings.time_before_change);
+					if(world.getWeatherDuration() != 0 || world.isThundering() && utils.Settings.cancel_rain) {
+						world.setWeatherDuration(0060);
+						world.setThunderDuration(0060);
+					}
 					utils.isReachedToNeeded = false;
 				} else {
 					utils.isReachedToNeeded = false;
 				}
 				} }, (utils.Settings.time_before_change * 20));
 		}
-		
 	}
-	
 }
--- src/me/elior/halfserversleep/utils/utils.java
+++ src/me/elior/halfserversleep/utils/utils.java
@@ -11,12 +11,13 @@ public class utils {
 	public static boolean isReachedToNeeded = false;
 
 	public static class Messages {
-		public static String player_went_to_sleep = ChatColor.translateAlternateColorCodes('&', Main.getPlugin(Main.class).getConfig().getString("Messages.player_went_to_sleep"));
-		public static String player_exited_from_sleep = ChatColor.translateAlternateColorCodes('&', Main.getPlugin(Main.class).getConfig().getString("Messages.player_exited_from_sleep"));
-		public static String all_needed_players_sleep = ChatColor.translateAlternateColorCodes('&', Main.getPlugin(Main.class).getConfig().getString("Messages.all_needed_players_sleep"));
+		public static String player_went_to_sleep = ChatColor.translateAlternateColorCodes('&', Main.getPlugin(Main.class).getConfig().getString("Messages.player-went-to-sleep"));
+		public static String player_exited_from_sleep = ChatColor.translateAlternateColorCodes('&', Main.getPlugin(Main.class).getConfig().getString("Messages.player-exited-from-sleep"));
+		public static String all_needed_players_sleep = ChatColor.translateAlternateColorCodes('&', Main.getPlugin(Main.class).getConfig().getString("Messages.all-needed-players-sleep"));
 	}
 	
 	public static class Settings {
+		public static Boolean cancel_rain = Main.getPlugin(Main.class).getConfig().getBoolean("Settings.cancel-rain");
 		public static long morning_time = Main.getPlugin(Main.class).getConfig().getLong("Settings.morning-time");
 		public static int time_before_change = Main.getPlugin(Main.class).getConfig().getInt("Settings.time-before-change");
 		public static String reguler_world = Main.getPlugin(Main.class).getConfig().getString("Settings.reguler-world");
